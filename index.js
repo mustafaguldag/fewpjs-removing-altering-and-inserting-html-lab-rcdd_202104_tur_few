5059eb0d60cc3184ca880b75a53e3b22869379e1
mustafaguldag <guldag.mustafaa@gmail.com>
2021-04-21 22:51:33 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "Musti is the champion" ;
